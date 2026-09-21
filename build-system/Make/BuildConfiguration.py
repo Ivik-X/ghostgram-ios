fccdd88611a85e5c2ae22fa014f2b611607e6dff
Ivik-X <pneginefim@gmail.com>
2026-09-22 00:27:04 +0400

    Fix bundle_id and add fallback for provisioning profiles and aps-environment

--- build-system/Make/BuildConfiguration.py
+++ build-system/Make/BuildConfiguration.py
@@ -173,6 +173,7 @@ def copy_profiles_from_directory(source_path, destination_path, team_id, bundle_
         '.BroadcastUpload': 'BroadcastUpload'
     }
 
+    copied_any = False
     for file_name in os.listdir(source_path):
         file_path = source_path + '/' + file_name
         if os.path.isfile(file_path):
@@ -196,8 +197,14 @@ def copy_profiles_from_directory(source_path, destination_path, team_id, bundle_
                 profile_base_name = profile_name[len(team_id + '.' + bundle_id):]
                 if profile_base_name in profile_name_mapping:
                     shutil.copyfile(file_path, destination_path + '/' + profile_name_mapping[profile_base_name] + '.mobileprovision')
+                    copied_any = True
                 else:
                     print('Warning: skipping provisioning profile at {} with bundle_id {} (base_name {})'.format(file_path, profile_name, profile_base_name))
+
+    if not copied_any:
+        for file_name in os.listdir(source_path):
+            if file_name.endswith('.mobileprovision'):
+                shutil.copyfile(source_path + '/' + file_name, destination_path + '/' + file_name)
 
 
 def resolve_aps_environment_from_directory(source_path, team_id, bundle_id):
@@ -227,7 +234,25 @@ def resolve_aps_environment_from_directory(source_path, team_id, bundle_id):
                         print('Provisioning profile at {} does not include an aps-environment entitlement'.format(file_path))
                         sys.exit(1)
                     return profile_dict['Entitlements']['aps-environment']
-    return None
+
+    for file_name in os.listdir(source_path):
+        if file_name.endswith('.mobileprovision'):
+            try:
+                profile_data = run_executable_with_output('openssl', arguments=[
+                    'smime',
+                    '-inform',
+                    'der',
+                    '-verify',
+                    '-noverify',
+                    '-in',
+                    source_path + '/' + file_name
+                ], decode=False, stderr_to_stdout=False, check_result=True)
+                profile_dict = plistlib.loads(profile_data)
+                if 'Entitlements' in profile_dict and 'aps-environment' in profile_dict['Entitlements']:
+                    return profile_dict['Entitlements']['aps-environment']
+            except Exception:
+                pass
+    return "production"
 
 
 def copy_certificates_from_directory(source_path, destination_path):
